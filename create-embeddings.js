3d4624de5c074b8a965d98170e6b973146b14c5e
Lukehsu <lukehsu1999@gmail.com>
2024-04-05 16:25:40 -0400

    import createEmbedding function into atlas-vector-search

--- create-embeddings.js
+++ create-embeddings.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const openai = new OpenAI({key: process.env.OPENAI_API_KEY});
 
 const createEmbedding = async (text) => {
+    console.log('text', text);
     const embedding = await openai.embeddings.create({
         model: "text-embedding-ada-002",
         input: text,
@@ -14,11 +15,11 @@ const createEmbedding = async (text) => {
 
   return embedding
 }
-createEmbedding('Once upon a time, there was').then(embedding => {
-    console.log('Received embedding:', embedding, "embedding shape", embedding.data[0].embedding);
-    const embeddingArray = embedding.data[0].embedding;
-    console.log('embeddingArray', embeddingArray.length);
-  }).catch(error => {
-    console.error('Error generating embedding:', error);
-  });
+// createEmbedding('Once upon a time, there was').then(embedding => {
+//     console.log('Received embedding:', embedding, "embedding shape", embedding.data[0].embedding);
+//     const embeddingArray = embedding.data[0].embedding;
+//     console.log('embeddingArray', embeddingArray.length);
+//   }).catch(error => {
+//     console.error('Error generating embedding:', error);
+//   });
 module.exports = { createEmbedding }
